7d46370c26c018e7d91b6f3fc1917894ff217834
Minhyek Jeon <71843044+mhj0326@users.noreply.github.com>
2024-07-29 15:36:54 -0400

    Add files via upload

---
+++ terminal.sh
@@ -0,0 +1,5 @@
+#Symbolic link for the file
+ln -s /home/user/original_file.txt /home/user/link_to_original_file.txt
+
+#Symbolic link for the directory
+ln -s /home/user/original_directory /home/user/link_to_original_directory
