45a82f9d10966dfda478dc98144401db9851bbab
Minhyek Jeon <71843044+mhj0326@users.noreply.github.com>
2024-09-18 14:49:44 -0400

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,2 +1,13 @@
 #new session in tmux
 tmux new-session -s <세션명>
+
+#tmux에서 현재 세션에 새로운 페이지 만들기
+#새로운 윈도우(페이지) 생성:
+Ctrl + b를 누른 뒤 c 키를 누릅니다.
+
+#윈도우 간 이동:
+Ctrl + b, + 0, 1, 2
+Ctrl + b, + p(이전 윈도우) 또는 n(다음 윈도우)
+
+#윈도우 목록 보기:
+Ctrl + b, w
