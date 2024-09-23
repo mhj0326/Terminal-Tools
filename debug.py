b688ea182ac2f4ab021d5af0c839dc38e9c63534
Minhyek Jeon <71843044+mhj0326@users.noreply.github.com>
2024-09-23 00:15:56 -0400

    Create debug.py

---
+++ debug.py
@@ -0,0 +1,8 @@
+import pdb
+pdb.set_trace()
+
+
+#To exit pdb:
+#Continue Execution: Type c to continue running the code.
+#Quit Debugger: Type q to quit pdb and stop execution.
+#Step Over: Type n to go to the next line.
