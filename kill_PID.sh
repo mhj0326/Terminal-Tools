5161f2914829624453bf8a5b64bec09c0af40dc9
Minhyek Jeon <71843044+mhj0326@users.noreply.github.com>
2024-10-13 19:28:52 -0400

    Update kill_PID.sh

--- kill_PID.sh
+++ kill_PID.sh
@@ -1,6 +1,9 @@
 kill -9 PID
 The kill -9 command is used to forcefully terminate a process in Unix-like operating systems (such as Linux or macOS). Here's what it does:
+ps -p [PID] -o user=
 
 kill: This command sends a signal to a process, instructing it to perform a certain action, typically to terminate.
 -9 (SIGKILL): This is the signal number that tells the operating system to immediately and forcefully stop the process. The -9 signal (also known as SIGKILL) cannot be ignored or handled by the process, ensuring that the process is killed instantly.
 In summary, kill -9 is a command used to forcefully kill processes that may not be responding to other signals or are stuck in a state where they cannot terminate normally.
+
+
